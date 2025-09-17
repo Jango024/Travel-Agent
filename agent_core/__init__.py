b98f0e4b4980f21f43e7c21a99e66f0f228b9f25
Jango024 <c.janker91@gmail.com>
2025-09-17 11:19:00 +0200

    Update __init__.py

--- agent_core/__init__.py
+++ agent_core/__init__.py
@@ -1,1 +1,12 @@
+"""Agent core package exposing reusable workflows."""
+from .config import AgentConfig, create_config, create_config_from_form, create_config_from_text
+from .workflow import AgentResult, run_agent_workflow
 
+__all__ = [
+    "AgentConfig",
+    "AgentResult",
+    "create_config",
+    "create_config_from_form",
+    "create_config_from_text",
+    "run_agent_workflow",
+]
